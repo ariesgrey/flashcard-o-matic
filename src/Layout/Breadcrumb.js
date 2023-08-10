01a563db1b35877cc400d16c761a1c2f77db9479
aries grey <ariesgrey88@gmail.com>
2023-08-10 18:28:29 -0500

    completed breadcrumb component

--- src/Layout/Breadcrumb.js
+++ src/Layout/Breadcrumb.js
@@ -1,8 +1,32 @@
-function Breadcrumb() {
+import { Link } from "react-router-dom";
+
+function Breadcrumb({ crumbs, links }) {
 	return (
-		<>
-			<h1>Breadcrumb</h1>
-		</>
+		<nav aria-label="breadcrumb">
+			<ol className="breadcrumb">
+				{/* 'Home' included in every instance */}
+				<li className="breadcrumb-item">
+					<Link to="/">
+						<i class="bi bi-house-fill"></i>Home
+					</Link>
+				</li>
+				{crumbs.map((crumb, index) => {
+					if (index === crumbs.length - 1) {
+						return (
+							<li className="breadcrumb-item active" aria-current="page">
+								{crumb}
+							</li>
+						);
+					} else {
+						return (
+							<li className="breadcrumb-item">
+								<Link to={links[index]}>{crumb}</Link>
+							</li>
+						);
+					}
+				})}
+			</ol>
+		</nav>
 	);
 }
 
