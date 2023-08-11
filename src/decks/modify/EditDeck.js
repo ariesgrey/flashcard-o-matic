c0ff33893116d3ba7eb4e5b9d97a89ba51859f11
aries grey <133818769+ariesgrey@users.noreply.github.com>
2023-08-11 11:25:32 -0500

    created EditDeck, not yet tested

--- src/decks/modify/EditDeck.js
+++ src/decks/modify/EditDeck.js
@@ -1,10 +1,61 @@
+import { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { readDeck, updateDeck } from "../../utils/api";
+import Breadcrumb from "../../Layout/Breadcrumb";
 import DeckForm from "./DeckForm";
 
-function EditDeck() {
+function EditDeck({ deck, setDeck, formData, setFormData, handleChange }) {
+	const { deckId } = useParams();
+
+	useEffect(() => {
+		async function loadDeck() {
+			const loadedDeck = await readDeck(deckId);
+			setDeck(loadedDeck);
+		}
+		loadDeck();
+	}, [deckId]);
+	
+	const crumbs = [deck.name, "Edit Deck"];
+	const links = [`/decks/${deckId}`];
+
+	const initialFormState = {
+		name: deck.name,
+		description: deck.description,
+	};
+	setFormData(initialFormState);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Check if this works or if formData needs to be split up
+		setDeck({
+			...deck,
+			formData,
+		});
+		async function editDeck() {
+			try {
+				await updateDeck(deck);
+				history.push(`/decks/${deckId}`);
+				//Check if force reload needed
+				//window.location.reload();
+			} catch (error) {
+				if (error.name !== "AbortError") {
+					throw error;
+				}
+			}
+		editDeck();
+		}
+	
 	return (
 		<>
-			<h1>EditDeck</h1>
-			<DeckForm />
+			<Breadcrumb crumbs={crumbs} links={links} />
+			<div className="my-3">
+				<h1>Edit Deck</h1>
+			</div>
+			<DeckForm
+				formData={formData}
+				handleChange={handleChange}
+				handleSubmit={handleSubmit}
+			/>
 		</>
 	);
 }
