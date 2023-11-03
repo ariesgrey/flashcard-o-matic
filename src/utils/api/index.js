cf47fe4795be8ca38b37515cda2a222b95e33c94
aries grey <ariesgrey88@gmail.com>
2023-11-02 22:09:05 -0500

    connected to separate json server

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,7 +2,8 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = "http://localhost:8080";
+// Created separate json server for live site
+const API_BASE_URL = "https://flashcard-o-matic-json-server.onrender.com/";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
